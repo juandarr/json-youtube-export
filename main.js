a2bbfca20033880bba6608e3123e868e915e46c9
Juan Ríos <juandarr@gmail.com>
2021-08-31 01:52:57 -0500

    feat: new functions that convers list of subscriptions in format <name url> to valid json to import in NewPipe

--- main.js
+++ main.js
@@ -1,38 +1,67 @@
-const npe = document.querySelector("#newpipe-export");
-const yte = document.querySelector("#youtube-export")
-const npi = document.querySelector("#newpipe-import");
-const btnCsv = document.querySelector("#from-csv");
-const btnJson = document.querySelector("#from-json");
+$(document).ready(function() {
+    const npe = document.getElementById("newpipe-export");
+    const yte = document.getElementById("youtube-export");
+    const npi = document.getElementById("newpipe-import");
+    const btnCsv = document.getElementById("from-csv");
+    const btnJson = document.getElementById("from-json");
+    const btnRow = document.getElementById("from-name-url");
 
-function fromJSON(e) {
-    e.preventDefault();
+    function fromJSON(e) {
+        e.preventDefault();
 
-    const subs = JSON.parse(yte.value);
-    const tmpl = JSON.parse(npe.value);
+        const subs = JSON.parse(yte.value);
+        const tmpl = JSON.parse(npe.value);
 
-    tmpl.subscriptions = subs;
-    npi.value = JSON.stringify(tmpl, null, 2);
-}
+        tmpl.subscriptions = subs;
+        npi.value = JSON.stringify(tmpl, null, 2);
+    }
 
-function fromCSV(e) {
-    e.preventDefault();
+    function fromCSV(e) {
+        e.preventDefault();
 
-    const offset = yte.value.startsWith("Channel ID") ? 1 : 0;
-    const subs = yte.value
-        .split("\n")
-        .slice(offset)
-        .map(line => line.trim())
-        .filter(line => line.length > 0)
-        .map(line => line.split(","))
-        .map(([, url, name]) => ({ service_id: 0, url: url.replace("http:", "https:"), name }));
+        const offset = yte.value.startsWith("Channel Id") ? 1 : 0;
+        const subs = yte.value
+            .split("\n")
+            .slice(offset)
+            .map((line) => line.trim())
+            .filter((line) => line.length > 0)
+            .map((line) => line.split(","))
+            .map(([, url, name]) => ({
+                service_id: 0,
+                url: url.replace("http:", "https:"),
+                name,
+            }));
 
+        const tmpl = JSON.parse(npe.value);
 
-    const tmpl = JSON.parse(npe.value);
+        tmpl.subscriptions = subs;
+        npi.value = JSON.stringify(tmpl, null, 2);
+    }
 
-    tmpl.subscriptions = subs;
-    npi.value = JSON.stringify(tmpl, null, 2);
+    function fromNameUrl(e) {
+        e.preventDefault();
 
-}
+        const subs = yte.value
+            .split("\n")
+            .map((line) => line.trim())
+            .filter((line) => line.length > 0)
+            .map((line) => line.split(/(http)/))
+            .map((item, index) => item.concat(index))
+            .map(([name, prefix, url, id]) => ({
+                service_id: id,
+                url: (prefix + url).replace("http:", "https:"),
+                name: name.trim(),
+            }));
 
-btnCsv.addEventListener("click", fromCSV);
-btnJson.addEventListener("click", fromJSON);
+        const tmpl = JSON.parse(npe.value);
+
+        tmpl.subscriptions = subs;
+        npi.value = JSON.stringify(tmpl, null, 2);
+    }
+
+    btnCsv.addEventListener("click", fromCSV);
+    btnJson.addEventListener("click", fromJSON);
+    btnRow.addEventListener("click", fromNameUrl);
+
+    console.log("Event listeners are ready!");
+});
